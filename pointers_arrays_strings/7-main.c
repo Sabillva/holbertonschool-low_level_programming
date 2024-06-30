db0115787977649a75ea63d7919818ac0f2a8dce
Sabillva <itifstudentsabina@gmail.com>
2024-06-30 13:24:31 +0000

    functionsnestedloops

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -7,9 +7,8 @@
  */
 int main(void)
 {
-char *str;
 
-str = "0123456789";
+char *str = "0123456789";
 puts_half(str);
 return (0);
 }
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,15 +1,28 @@
 #include "main.h"
-#include <string.h>
-/*for strlen function*/
 
 /**
- * puts_half - Prints the second half of a string, followed by a new line.
- * @str: The input string
+ * _strlen - Calculates the length of a string
+ * @str: The string to calculate the length of
+ *
+ * Return: The length of the string
  */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+
+return (len);
+}
 
 void puts_half(char *str)
 {
-int len = strlen(str);
+int len = _strlen(str);
 int i, start;
 
 if (len % 2 == 0)
