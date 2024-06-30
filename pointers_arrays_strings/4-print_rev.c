47b0336ced769f6da155ab2db0520d34c6722f68
Sabillva <itifstudentsabina@gmail.com>
2024-06-30 12:13:00 +0000

    functionsnestedloops

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,20 +8,17 @@ void print_rev(char *s)
 {
 int length = 0;
 int i;
+
 /*Calculate the length of the string*/
-while (*s != '\0')
+while (s[length] != '\0')
 {
 length++;
-s++;
 }
-/*Move s back to the start of the string*/
-s--;
 
 /*Print characters in reverse order*/
 for (i = length - 1; i >= 0; i--)
 {
-_putchar(*(s - i));
+_putchar(s[i]);
 }
 _putchar('\n');
 }
-
