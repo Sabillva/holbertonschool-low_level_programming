16c7cfd83752392f5618c66bb9b61e7f8ab6166e
Sabillva <itifstudentsabina@gmail.com>
2024-06-30 13:35:32 +0000

    functionsnestedloops

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen - Calculates the length of a string
- * @str: The string to calculate the length of
+ * _strlen - Calculate the length of a string
+ * @s: Pointer to the string
  *
- * Return: The length of the string
+ * Return: The length of the string (number of characters excluding null byte)
  */
-
 int _strlen(char *s)
 {
 int len = 0;
-
 while (*s != '\0')
 {
 len++;
@@ -20,6 +18,10 @@ s++;
 return (len);
 }
 
+/**
+ * puts_half - Prints the second half of a string
+ * @str: The string to process
+ */
 void puts_half(char *str)
 {
 int len = _strlen(str);
@@ -31,7 +33,7 @@ start = len / 2;
 }
 else
 {
-start = (len + 1) / 2;
+start = (len - 1) / 2;
 }
 
 for (i = start; i < len; i++)
