e28acc033cc52ad9e49e0e6dd1174974343acae7
Sabillva <itifstudentsabina@gmail.com>
2024-07-01 11:08:42 +0000

    pointersandarrays

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,6 +5,7 @@
  * @s: pointer to the memory area
  * @b: constant byte
  * @n: number of bytes to fill
+ * Return: pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
