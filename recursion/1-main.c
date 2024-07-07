23bd9894ae6c3b36b22ac4274dff6cd93e7bdc3d
Sabillva <itifstudentsabina@gmail.com>
2024-07-07 21:56:43 +0000

    cisfun

---
+++ recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Test function for _print_rev_recursion
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+_print_rev_recursion("\nColton Walker");
+return (0);
+}
+
