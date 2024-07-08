97e0479670d88e2502859ac11efaf34230546c83
Sabillva <itifstudentsabina@gmail.com>
2024-07-08 13:19:04 +0000

    cisfun

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments passed to the program
+ * @argv: Array of pointers to command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+/*Prevent unused parameter warning for argc*/
+(void)argc;
+
+/*Print the program name stored in argv[0]*/
+printf("%s\n", argv[0]);
+
+return (0);
+}
+
---
+++ argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Function prototype */
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
+
