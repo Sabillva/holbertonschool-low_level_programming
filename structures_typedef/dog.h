773234df86be5ae87af5a1082f03f1a3d214008c
Sabillva <itifstudentsabina@gmail.com>
2024-07-14 11:39:02 +0000

    mallocandcolloc

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct that defines a dog with a name, age, and owner.
+ * @name: Name of the dog, type = char *
+ * @age: Age of the dog, type = float
+ * @owner: Owner of the dog, type = char *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
