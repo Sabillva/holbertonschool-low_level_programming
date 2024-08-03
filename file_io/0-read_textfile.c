f44bdeff185118a1eaba7fb7731c2934a7f0e96e
Sabillva <itifstudentsabina@gmail.com>
2024-08-03 14:15:29 +0000

    added new files

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,47 +11,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nread, nwrite;
-    char *buffer;
+	int fd;
+	ssize_t nread, nwrite;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    /* Allocate memory for buffer */
-    buffer = malloc(letters);
-    if (buffer == NULL)
-        return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
 
-    /* Open the file for reading */
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-    {
-        free(buffer);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    /* Read from the file */
-    nread = read(fd, buffer, letters);
-    if (nread < 0)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
+	nread = read(fd, buffer, letters);
+	if (nread < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-    /* Write to stdout */
-    nwrite = write(STDOUT_FILENO, buffer, nread);
-    if (nwrite < 0 || nwrite != nread)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
+	nwrite = write(STDOUT_FILENO, buffer, nread);
+	if (nwrite < 0 || nwrite != nread)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-    /* Clean up and return number of letters read and printed */
-    close(fd);
-    free(buffer);
-    return (nwrite);
+	close(fd);
+	free(buffer);
+	return (nwrite);
 }
 
